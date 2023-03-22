c06f244f52d9fcb3db0490a2bc1cb86306ec2c17
antony.felisardo <antony.felisardo@csxinovacao.com.br>
2023-03-22 15:33:54 -0300

    card adicionado

--- src/app/models/car.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Car {
-  brand: string;
-  year: string;
-  color: string;
-  vin: string;
-}
--- src/app/pages/brand/brand.component.ts
+++ src/app/pages/brand/brand.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Car } from 'src/app/models/car.model';
-import { CarService } from 'src/app/services/cars.service';
 
 @Component({
   selector: 'app-brand',
@@ -8,27 +6,7 @@ import { CarService } from 'src/app/services/cars.service';
   styleUrls: ['./brand.component.css']
 })
 export class BrandComponent {
-
-  cars: Car[] =
-    [
-      { brand: 'Volkswagen', year: '2012', color: 'White', vin: 'dsad231ff' },
-      { brand: 'Audi', year: '2011', color: 'Black', vin: 'gwregre345' },
-      { brand: 'Renault', year: '2005', color: 'Gray', vin: 'h354htr' },
-      { brand: 'BMW', year: '2003', color: 'Blue', vin: 'j6w54qgh' },
-      { brand: 'Mercedes', year: '1995', color: 'White', vin: 'hrtwy34' },
-      { brand: 'Volvo', year: '2005', color: 'Black', vin: 'jejtyj' },
-      { brand: 'Honda', year: '2012', color: 'Yellow', vin: 'g43gr' },
-      { brand: 'Jaguar', year: '2013', color: 'White', vin: 'greg34' },
-      { brand: 'Ford', year: '2000', color: 'Black', vin: 'h54hw5' },
-      { brand: 'Fiat', year: '2013', color: 'Red', vin: '245t2s' }
-    ]
-
-
-    ;
-
-//  constructor(private carService: CarService) { }
-
-  //  ngOnInit() {
-  //      this.carService.getCarsSmall().then(cars => this.cars = cars);
-  //  }
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
 }

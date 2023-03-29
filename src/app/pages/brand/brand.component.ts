571aa8132a5836bc4f1c072b97fc0fbe8ce3f19e
Fernanda Costa <ext_fernanda.costa@csxinovacao.com.br>
2023-03-29 09:46:57 -0300

    fix: correções para usar o firestore no service

--- src/app/pages/brand/brand.component.ts
+++ src/app/pages/brand/brand.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Firestore, collectionData, collection, addDoc } from '@angular/fire/firestore';
+import { BuyerService } from 'src/app/services/buyer.service';
 
 @Component({
   selector: 'app-brand',
@@ -8,19 +8,15 @@ import { Firestore, collectionData, collection, addDoc } from '@angular/fire/fir
   styleUrls: ['./brand.component.css']
 })
 export class BrandComponent {
-  item$: Observable<any[]>;
+  item$?: Observable<any[]>;
   data = {
     name: 'Los Angeles 123',
     state: 'CA 123',
     country: 'USA 123'
   };
 
-  constructor(firestore: Firestore) {
-    const dados = collection(firestore, 'items');
-    this.item$ = collectionData(dados);
-
-    const ref = collection(firestore, 'dados');
-    addDoc(ref, this.data);
+  constructor( private buyerSerivce: BuyerService) {
+    this.item$ =  this.buyerSerivce.getList();
   }
 
 }
